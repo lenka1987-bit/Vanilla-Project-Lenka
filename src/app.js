15e51aade5d101df0c38390180c924acebffeda3
LenkaDoubravova <lenka.doubravova@gmail.com>
2020-10-02 19:01:48 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+
   let days = [
     "Sunday",
     "Monday",
@@ -19,7 +12,7 @@ function formatDate(timestamp) {
   ];
 
   let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  return `${day} ${formatHours(timestamp)}`;
 }
 
 function formatHours(timestamp) {
@@ -34,20 +27,6 @@ function formatHours(timestamp) {
   }
   return `${hours}:${minutes}`;
 }
-/*
-function getIcon(icon) {
-     class = "";
-    if (icon === "01d") {
-        class = "fa-sun";
-    } else if (icon === "02d") {
-        class = "fa-cloud-sun";
-    } else if (icon === "03d" || icon === "04d") {
-        class = "fa-cloud";
-    } 
- 
-    return class;
-}
-*/
 
 function showData(response) {
   console.log(response.data);
@@ -59,18 +38,90 @@ function showData(response) {
   let date = document.querySelector("#date");
   let sunrise = document.querySelector("#sunrise");
   let sunset = document.querySelector("#sunset");
+  let iconElement = document.querySelector("#icon");
 
   showWeather.innerHTML = response.data.weather[0].description;
   showCity.innerHTML = response.data.name;
-  showTemperature.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemperature = Math.round(response.data.main.temp);
+  showTemperature.innerHTML = celsiusTemperature;
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
   date.innerHTML = formatDate(response.data.dt * 1000);
   sunrise.innerHTML = formatHours(response.data.sys.sunrise * 1000);
   sunset.innerHTML = formatHours(response.data.sys.sunset * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
+// working on forecast
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = null;
+  forecastElement.innerHTML = null;
+  for (let index = 0; index < 5; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `<div class="col-2">
+              <ul>
+                <li>${formatHours(forecast.dt * 1000)}</li>
+                <li id="current-weather-icon"><img
+                    src="http://openweathermap.org/img/wn/${
+                      forecast.weather[0].icon
+                    }@2x.png"
+                    alt="weather-icon"
+                    class="weather-icon weather-icon-forecast"
+                    id="icon" /></li>
+                <li class="max-temp"><strong>${Math.round(
+                  forecast.main.temp_max
+                )}℃</strong></li>
+              </ul>
+            </div>`;
+  }
+}
+// working on searching engine
+
+function search(city) {
+  let apiKey = "64e05917145944cb1b0f8609f542a318";
+  let apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiCall).then(showData);
+
+  apiCall = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiCall).then(displayForecast);
 }
 
-let apiKey = "64e05917145944cb1b0f8609f542a318";
-let apiCall = `https://api.openweathermap.org/data/2.5/weather?q=Piestany&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#search-city");
+  search(cityInputElement.value);
+}
 
-axios.get(apiCall).then(showData);
+let form = document.querySelector("#search-engine");
+form.addEventListener("submit", handleSubmit);
+
+search("Piešťany");
+
+// convert units
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  celsius.classList.remove("active");
+  fahrenheit.classList.add("active");
+  let temperatureElement = document.querySelector("#temp");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null; //global variable
+
+let fahrenheit = document.querySelector("#fahrenheit-unit");
+fahrenheit.addEventListener("click", convertToFahrenheit);
+
+let celsius = document.querySelector("#celsius-unit");
+celsius.addEventListener("click", convertToCelsius);
